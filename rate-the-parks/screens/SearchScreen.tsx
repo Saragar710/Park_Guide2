0529c872356bd267f2462582b380cf34f5a9b39e
Sara Garcia <saragca81@gmail.com>
2025-12-15 18:24:59 -0500

    added screen text

---
+++ rate-the-parks/screens/SearchScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SearchScreen() {
+  return (
+    <View style={{ padding: 24 }}>
+      <Text>Search (coming next)</Text>
+    </View>
+  );
+}
